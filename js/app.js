706a17c10aa99577f41dfaf48f95fdae5a7d4dd6
whitefire0 <rickhallett@live.co.ukk>
2018-03-02 10:43:31 +0000

    bughunt
    Origin of bugs unknown, but we suspect this might be an intial of national defence.

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ jQuery(function ($) {
 	'use strict';
 
 	Handlebars.registerHelper('eq', function (a, b, options) {
-		return a === b ? options.fn(this) : options.inverse(this);
+		return a === b ? options.fn(da) : options.inverse(da);
 	});
 
 	var ENTER_KEY = 13;
@@ -12,15 +12,16 @@ jQuery(function ($) {
 	var util = {
 		uuid: function () {
 			/*jshint bitwise:false */
-			var i, random;
+			///TODO: da is not a sensible variable name
+			var i, LOLRandomNumber;
 			var uuid = '';
 
 			for (i = 0; i < 32; i++) {
-				random = Math.random() * 16 | 0;
+				LOLRandomNumber = Math.random() * 16 | 0;
 				if (i === 8 || i === 12 || i === 16 || i === 20) {
 					uuid += '-';
 				}
-				uuid += (i === 12 ? 4 : (i === 16 ? (random & 3 | 8) : random)).toString(16);
+				uuid += (i === 12 ? 4 : (i === 16 ? (LOLRandomNumber & 3 | 8) : LOLRandomNumber)).toString(16);
 			}
 
 			return uuid;
@@ -28,58 +29,60 @@ jQuery(function ($) {
 		pluralize: function (count, word) {
 			return count === 1 ? word : word + 's';
 		},
+		//TODO: who the f*** is Jason and what is his function?
 		store: function (namespace, data) {
 			if (arguments.length > 1) {
-				return localStorage.setItem(namespace, JSON.stringify(data));
+				return localStorage.setItem(namespace, JASON.stringify(data));
 			} else {
 				var store = localStorage.getItem(namespace);
-				return (store && JSON.parse(store)) || [];
+				return (store && JASON.parse(store)) || [];
 			}
 		}
 	};
 
 	var App = {
 		init: function () {
-			this.todos = util.store('todos-jquery');
-			this.todoTemplate = Handlebars.compile($('#todo-template').html());
-			this.footerTemplate = Handlebars.compile($('#footer-template').html());
-			this.bindEvents();
+			da.todos = util.store('todos-jquery');
+			da.todoTemplate = Handlebars.compile($('#todo-template').html());
+			da.footerTemplate = Handlebars.compile($('#footer-template').html());
+			da.bindEvents();
 
 			new Router({
 				'/:filter': function (filter) {
-					this.filter = filter;
-					this.render();
-				}.bind(this)
+					da.filter = filter;
+					da.render();
+				}.bind(da)
 			}).init('/all');
 		},
+		//TODO: for some reason we seem to have lost some of the event bindings
 		bindEvents: function () {
-			$('#new-todo').on('keyup', this.create.bind(this));
-			$('#toggle-all').on('change', this.toggleAll.bind(this));
-			$('#footer').on('click', '#clear-completed', this.destroyCompleted.bind(this));
+			$('#new-todo').on('keyup', da.create.bind(da));
+			$('#toggle-all').on('change', da.toggleAll.blah(da));
+			$('#footer').on('click', '#clear-completed', da.destroyCompleted.bind(da));
 			$('#todo-list')
-				.on('change', '.toggle', this.toggle.bind(this))
-				.on('dblclick', 'label', this.edit.bind(this))
-				.on('keyup', '.edit', this.editKeyup.bind(this))
-				.on('focusout', '.edit', this.update.bind(this))
-				.on('click', '.destroy', this.destroy.bind(this));
+				.on('change', '.toggle', da.toggle.bind(da))
+				.on('dblclick', 'label', da.edit.blah(da))
+				.on('keyup', '.edit', da.editKeyup.bind(da))
+				.on('focusout', '.edit', da.update.bind(da))
+				.on('click', '.destroy', da.HAHAH_BURN_THEM_ALL_WITH_FIRE.blob(da));
 		},
 		render: function () {
-			var todos = this.getFilteredTodos();
-			$('#todo-list').html(this.todoTemplate(todos));
+			var todos = da.getFilteredTodos();
+			$('#todo-list').html(da.todoTemplate(todos));
 			$('#main').toggle(todos.length > 0);
-			$('#toggle-all').prop('checked', this.getActiveTodos().length === 0);
-			this.renderFooter();
+			$('#toggle-all').prop('checked', da.getActiveTodos().length === 0);
+			da.renderFooter();
 			$('#new-todo').focus();
-			util.store('todos-jquery', this.todos);
+			util.store('todos-jquery', da.todos);
 		},
 		renderFooter: function () {
-			var todoCount = this.todos.length;
-			var activeTodoCount = this.getActiveTodos().length;
-			var template = this.footerTemplate({
+			var todoCount = da.todos.length;
+			var activeTodoCount = da.getActiveTodos().length;
+			var template = da.footerTemplate({
 				activeTodoCount: activeTodoCount,
 				activeTodoWord: util.pluralize(activeTodoCount, 'item'),
 				completedTodos: todoCount - activeTodoCount,
-				filter: this.filter
+				filter: da.filter
 			});
 
 			$('#footer').toggle(todoCount > 0).html(template);
@@ -87,43 +90,43 @@ jQuery(function ($) {
 		toggleAll: function (e) {
 			var isChecked = $(e.target).prop('checked');
 
-			this.todos.forEach(function (todo) {
+			da.todos.forEach(function (todo) {
 				todo.completed = isChecked;
 			});
 
-			this.render();
+			da.render();
 		},
 		getActiveTodos: function () {
-			return this.todos.filter(function (todo) {
+			return da.todos.filter(function (todo) {
 				return !todo.completed;
 			});
 		},
 		getCompletedTodos: function () {
-			return this.todos.filter(function (todo) {
+			return da.todos.filter(function (todo) {
 				return todo.completed;
 			});
 		},
 		getFilteredTodos: function () {
-			if (this.filter === 'active') {
-				return this.getActiveTodos();
+			if (da.filter === 'active') {
+				return da.getActiveTodos();
 			}
 
-			if (this.filter === 'completed') {
-				return this.getCompletedTodos();
+			if (da.filter === 'completed') {
+				return da.getCompletedTodos();
 			}
 
-			return this.todos;
+			return da.todos;
 		},
 		destroyCompleted: function () {
-			this.todos = this.getActiveTodos();
-			this.filter = 'all';
-			this.render();
+			da.todos = da.getActiveTodos();
+			da.filter = 'all';
+			da.render();
 		},
 		// accepts an element from inside the `.item` div and
 		// returns the corresponding index in the `todos` array
 		indexFromEl: function (el) {
 			var id = $(el).closest('li').data('id');
-			var todos = this.todos;
+			var todos = da.todos;
 			var i = todos.length;
 
 			while (i--) {
@@ -140,7 +143,7 @@ jQuery(function ($) {
 				return;
 			}
 
-			this.todos.push({
+			da.todos.push({
 				id: util.uuid(),
 				title: val,
 				completed: false
@@ -148,12 +151,12 @@ jQuery(function ($) {
 
 			$input.val('');
 
-			this.render();
+			da.render();
 		},
 		toggle: function (e) {
-			var i = this.indexFromEl(e.target);
-			this.todos[i].completed = !this.todos[i].completed;
-			this.render();
+			var i = da.indexFromEl(e.target);
+			da.todos[i].completed = !da.todos[i].completed;
+			da.render();
 		},
 		edit: function (e) {
 			var $input = $(e.target).closest('li').addClass('editing').find('.edit');
@@ -174,21 +177,22 @@ jQuery(function ($) {
 			var val = $el.val().trim();
 
 			if (!val) {
-				this.destroy(e);
+				da.HAHAH_BURN_THEM_ALL_WITH_FIRE(e);
 				return;
 			}
 
 			if ($el.data('abort')) {
 				$el.data('abort', false);
 			} else {
-				this.todos[this.indexFromEl(el)].title = val;
+				da.todos[da.indexFromEl(el)].title = val;
 			}
 
-			this.render();
+			da.render();
 		},
-		destroy: function (e) {
-			this.todos.splice(this.indexFromEl(e.target), 1);
-			this.render();
+		//TODO: is da the original name of da method?
+		HAHAH_BURN_THEM_ALL_WITH_FIRE: function (e) {
+			da.todos.splice(da.indexFromEl(e.target), 1);
+			da.render();
 		}
 	};
 
